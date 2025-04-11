078492baa147518f8d976b9bdfc04df72204f90b
aarogyaojha <arogyaojha251@gmail.com>
2025-04-11 22:47:51 +0530

    chnaging from row to listtile

--- lib/main.dart
+++ lib/main.dart
@@ -47,21 +47,18 @@ class MyApp extends StatelessWidget {
                 margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Icon(Icons.phone, color: Colors.teal, size: 18),
-                      SizedBox(width: 10),
-                      Text(
-                        "+91 620 658 2556",
-                        style: TextStyle(
-                          color: Colors.teal[900],
-                          fontFamily: "Source Sans",
-                          fontSize: 15,
-                          fontWeight: FontWeight.bold,
-                          letterSpacing: 2.5,
-                        ),
+                  child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal, size: 18),
+                    title: Text(
+                      "+91 620 658 2556",
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: "Source Sans",
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                        letterSpacing: 2.5,
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
@@ -70,20 +67,17 @@ class MyApp extends StatelessWidget {
                 color: Colors.white,
                 child: Padding(
                   padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Icon(Icons.email, color: Colors.teal),
-                      SizedBox(width: 10),
-                      Text(
-                        "arogyaojha251@gmail.com",
-                        style: TextStyle(
-                          fontFamily: "Source Sans",
-                          letterSpacing: 2.5,
-                          color: Colors.teal[900],
-                          fontWeight: FontWeight.bold,
-                        ),
+                  child: ListTile(
+                    leading: Icon(Icons.email, color: Colors.teal),
+                    title: Text(
+                      "arogyaojha251@gmail.com",
+                      style: TextStyle(
+                        fontFamily: "Source Sans",
+                        letterSpacing: 2.5,
+                        color: Colors.teal[900],
+                        fontWeight: FontWeight.bold,
                       ),
-                    ],
+                    ),
                   ),
                 ),
               ),
