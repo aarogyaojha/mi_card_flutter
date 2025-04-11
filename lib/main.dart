8ede866624af668ddd9e464aa7b4dfaba27a1e76
aarogyaojha <arogyaojha251@gmail.com>
2025-04-11 20:23:14 +0530

    added some containers and icons

--- lib/main.dart
+++ lib/main.dart
@@ -42,6 +42,46 @@ class MyApp extends StatelessWidget {
                   letterSpacing: 2.5,
                 ),
               ),
+              Container(
+                color: Colors.white,
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                child: Row(
+                  children: [
+                    Icon(Icons.phone, color: Colors.teal, size: 18),
+                    SizedBox(width: 10),
+                    Text(
+                      "+91 620 658 2556",
+                      style: TextStyle(
+                        color: Colors.teal[900],
+                        fontFamily: "Source Sans",
+                        fontSize: 15,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 25),
+                padding: EdgeInsets.all(10),
+                color: Colors.white,
+                child: Row(
+                  children: [
+                    Icon(Icons.email, color: Colors.teal),
+                    SizedBox(width: 10),
+                    Text(
+                      "arogyaojha251@gmail.com",
+                      style: TextStyle(
+                        fontFamily: "Source Sans",
+                        letterSpacing: 2.5,
+                        color: Colors.teal[900],
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
             ],
           ),
         ),
